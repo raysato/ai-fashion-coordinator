1f2d950e2d11a6bb56b3cf2f74dc94b6aa60a5b5
sweden <ray.likes.pasta@gmail.com>
2023-07-09 23:11:10 +0900

    fix mobile menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -56,10 +56,16 @@ const user = useSupabaseUser()
           </div> 
           <div class="drawer-side">
             <label for="my-drawer-3" class="drawer-overlay"></label> 
-            <ul class="menu p-4 w-80 h-full bg-base-200">
-              <li><NuxtLink to="/logout">Logout</NuxtLink></li>
-            </ul>
-            
+            <div class="h-full flex flex-col w-80 bg-base-200  ">
+              <ul class="menu flex-auto p-4">
+                <li v-if="user"><NuxtLink to="/logout">Logout</NuxtLink></li>
+                <li v-else="user"><NuxtLink to="/login">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" /></svg>
+                  Login / Signup
+                </NuxtLink></li>
+              </ul>
+              <p v-if="user" class="p-4">{{ user.email }}</p>
+            </div>
           </div>
         </div>
       </Body>
