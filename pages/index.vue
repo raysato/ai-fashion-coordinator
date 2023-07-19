07d32ed6e4a75bfaa77db2a79af3bf9ad492ac88
sweden <ray.likes.pasta@gmail.com>
2023-07-19 18:32:18 +0900

    move login modal to login.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+definePageMeta({
+  middleware: 'auth'
+})
+
 const isLoggedIn = ref(false)
 const locationGranted = ref(false)
 const showModal = ref(false)
@@ -56,7 +60,7 @@ const closeModal = () => {
 </script>
 
 <template>
-  <div v-if="isLoggedIn" class="m-4">
+  <div  class="m-4">
     <div v-if="!locationGranted">
       <!-- Alert for location permission -->
       <div class="alert alert-info">
@@ -142,38 +146,6 @@ const closeModal = () => {
             <button type="button" class="btn" @click="closeModal">
               ✖️
             </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div v-else class="hero min-h-screen bg-gray-200">
-    <div class="hero-content flex-col"> <!-- Add flex-col here -->
-      <div class="text-center lg:text-left">
-        <h1 class="text-5xl font-bold">Login now!</h1>
-        <p class="py-6">Style thermoを使うにはログインが必要です</p>
-      </div>
-      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-
-        <div class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input type="text" placeholder="email" class="input input-bordered" />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input type="password" placeholder="password" class="input input-bordered" />
-            <label class="label">
-              <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-            </label>
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary" @click="login">Login</button>
           </div>
         </div>
       </div>
--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'plain',
   middleware: 'noauth'
 })
 const supabase = useSupabaseClient()
@@ -24,27 +23,36 @@ const handleLogin = async () => {
 </script>
 
 <template>
-<form @submit.prevent="handleLogin"  class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-    <div class="card-body">
-    <div class="form-control">
-        <label class="label">
-        <span class="label-text">Email</span>
-        </label>
-        <input v-model="email" type="email" placeholder="email" class="input input-bordered" />
+<div class="hero min-h-screen bg-gray-200">
+  <div class="hero-content flex-col"> <!-- Add flex-col here -->
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Login now!</h1>
+      <p class="py-6">Style thermoを使うにはログインが必要です</p>
     </div>
-    <div class="form-control">
-        <label class="label">
-        <span class="label-text">Password</span>
-        </label>
-        <input v-model="password" type="password" min="6" placeholder="password" class="input input-bordered" />
-        <label class="label">
-        <!-- <a href="#" class="label-text-alt link link-hover">Forgot password?</a> -->
-        <NuxtLink to="/signup" class="label-text-alt link link-hover link-primary">or Signup</NuxtLink>
-        </label>
-    </div>
-    <div class="form-control mt-6">
-        <button :disabled="loading" class="btn btn-primary">Login</button>
-    </div>
-    </div>
-  </form>
+    
+    <form @submit.prevent="handleLogin"  class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+        <div class="form-control">
+            <label class="label">
+            <span class="label-text">Email</span>
+            </label>
+            <input v-model="email" type="email" placeholder="email" class="input input-bordered" />
+        </div>
+        <div class="form-control">
+            <label class="label">
+            <span class="label-text">Password</span>
+            </label>
+            <input v-model="password" type="password" min="6" placeholder="password" class="input input-bordered" />
+            <label class="label">
+            <!-- <a href="#" class="label-text-alt link link-hover">Forgot password?</a> -->
+            <NuxtLink to="/signup" class="label-text-alt link link-hover link-primary">or Signup</NuxtLink>
+            </label>
+        </div>
+        <div class="form-control mt-6">
+            <button :disabled="loading" class="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
 </template>
